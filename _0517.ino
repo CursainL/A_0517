099192d59546eef1a95150bca99588a42ea3d20b
CursainL <otakuliu01@gmail.com>
2017-05-17 11:25:10 +0800

    First Commit

---
+++ _0517.ino
@@ -0,0 +1,29 @@
+const int analogPin = A0;   // the pin that the potentiometer is attached to
+const int ledCount = 10;    // the number of LEDs in the bar graph
+int ledPins[] = {
+  2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+};   // an array of pin numbers to which LEDs are attached
+void setup() {
+  // loop over the pin array and set them all to output:
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    pinMode(ledPins[thisLed], OUTPUT);
+  }
+}
+
+void loop() {
+  int dTime;
+  for(int i=0;i<=ledCount;i++) {
+    digitalWrite(ledPins[i],HIGH );
+    int sensorReading = analogRead(analogPin);
+    dTime = map(sensorReading , 0 , 900 , 10, 1000 );
+    delay(dTime);
+  };
+  for(int i=10;i>=0; i--) {
+    digitalWrite(ledPins[i] , LOW );
+    int sensorReading = analogRead(analogPin);
+    dTime = map(sensorReading , 0 , 900 , 10, 1000 );
+    delay(dTime); 
+  };
+}
+
+
